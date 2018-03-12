a20f3648067a2179d127ec8796f5b8eed2b2247d
Eric Solner <ericsolner2018@u.northwestern.edu>
2018-03-12 18:30:46 -0500

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
+  ("Big Baby", false, "2016-11-05 04:20:01"),
+  ("Pretzel Burger", false, "2016-11-05 04:20:02"),
+  ("Baconator", false, "2016-11-05 16:20:00"),
+  ("Butter Burger", false, "2016-11-05 16:20:01"),
+  ("McDouble", false, "2016-11-05 04:20:20"),
+  ("Triple Stack", true, "2016-11-05 16:20:16");
